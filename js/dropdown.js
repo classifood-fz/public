badd1edc2a843c44a88dee7df0be69f9797eabec
Felix Zheng <felix.zheng@classifood.com>
2014-10-24 00:38:05 -0400

    Overhauled menu system

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,11 +1,12 @@
 jQuery(document).ready(function() {
-    // Show and hide drop-down menu on hover
-    jQuery('#dropdown').hover(
-        function() {
+    // Show and hide drop-down menu
+    jQuery('#dropdown').click(function() {
+        if (jQuery('.dropdown').css('display') == 'none') {
             jQuery('.dropdown').css('display', 'inline-block');
-        },
-        function() {
+            jQuery(this).css('border', '1px solid #000000');
+        } else {
             jQuery('.dropdown').css('display', 'none');
+            jQuery(this).css('border', '0');
         }
-    );
+    });
 });
